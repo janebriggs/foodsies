afbb7004e9e468dd6f0d2ba692ff8092e0d7eaa3
Jane Briggs <briggs.jane@gmail.com>
2016-07-25 15:25:18 -0400

    font-color

--- menu.js
+++ menu.js
@@ -7,32 +7,29 @@ $(document).ready(function() {
 $(document).ready(function() {
     var primero = $("#Appetizers");
     primero.click(function(event) {
-        primero.css("color", "red");
         primero.css("font-weight", "bold");
         $(".treat, .dinner").hide();
         $(".apps").show();
-        primero.css ("color", "#696969");
+        primero.css ("color", "#99004d");
         primero.siblings().css("font-weight", "normal");
         $("#Yum").css("font-weight", "normal");
         //gets only the siblings to change, not the element itself
     });
     var segundo = $("#Mains");
     segundo.click(function(event) {
-        segundo.css("color", "red");
         segundo.css("font-weight", "bold");
         $(".treat, .apps").hide();
         $(".dinner").show();
-        segundo.css ("color", "#696969");
+        segundo.css ("color", "#99004d");
         segundo.siblings().css("font-weight", "normal");
         $("#Yum").css("font-weight", "normal");
     });
     var tercero = $("#Desserts");
     tercero.click(function(event) {
-        tercero.css("color", "red");
         tercero.css("font-weight", "bold");
         $(".dinner, .apps").hide();
         $(".treat").show();
-        tercero.css ("color", "#696969");
+        tercero.css ("color", "#99004d");
         tercero.siblings().css("font-weight", "normal");
         $("#Yum").css("font-weight", "normal");
     });
